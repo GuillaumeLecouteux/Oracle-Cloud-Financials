14c56d63164af19e457e693dc6dc96cb89ea8d9b
Guillaume Lecouteux <42238724+GuillaumeLecouteux@users.noreply.github.com>
2022-12-14 16:33:22 +0000

    Create Organizations.sql

---
+++ Oracle Cloud Fusion Financials ERP/SQL/Organizations.sql
@@ -0,0 +1,12 @@
+select xep.name as legal_entity_name
+, xep.legal_entity_id as legal_entity_id
+, hou.name  as busines_unit_name
+, hou.organization_id as org_id
+, geo.country_code as country_code
+, geo.geography_id as geography_id
+from fusion.xle_entity_profiles xep
+join fusion.hr_operating_units hou
+  on hou.default_legal_context_id = xep.legal_entity_id
+join fusion.hz_geographies geo
+  on geo.geography_id = xep.geography_id
+order by geo.country_code, xep.name
